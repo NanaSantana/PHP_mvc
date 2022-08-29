1d73ad9aa4ec26445449cbca4d5e6af36092048b
Nana <coloca o email>
2022-08-29 12:29:33 -0300

    add config e modificando autoload

--- App/autoload.php
+++ App/autoload.php
@@ -2,7 +2,18 @@
 
 //o autoload faz com que agnete nao precise mais do include, pq ai o php procurs rprwa gente
 //spl --> classesz internas do php ;D
+
 spl_autoload_register(function ($nome_da_classe) {
+
+    $arquivo = BASEDIR . '/' . $nome_da_classe . '.php';
+
+    if(file_exists($arquivo)){
+        include $arquivo;
+    } else
+    exit('Arquivo não encontrado. Arquivos:' . $arquivo ."<br/>");
+});
+
+/*spl_autoload_register(function ($nome_da_classe) {
 
     $classe = dirname(__FILE__) . '/../' . $nome_da_classe . ".php";
 
@@ -12,30 +23,4 @@ spl_autoload_register(function ($nome_da_classe) {
     else{
         echo 'arquivo não encontrado' . $classe;
         }
-
-    //echo "Tentou dar include de: " . $nome_da_classe;
-    //ele producz pra mim o arquiivo q eu queo dentor do mvc
-    //ele serve para o sistema inteiro por isso tem o nome_da_classe
-
-    /*$classe_controller = 'Controller/' . $nome_da_classe . ".php";
-    $classe_model = 'Model/' . $nome_da_classe . ".php";
-    $classe_dao = 'DAO/' . $nome_da_classe . ".php";
-
-    //se o aquivo pipi existir ele da o include
-    //se naão existir ele vai pro de baicos
-
-    if(file_exists($classe_controller))
-    {
-        include $classe_controller;
-
-    } else if(file_exists($classe_model)) {
-
-        include $classe_model;
-
-    } else if(file_exists($classe_dao)) {
-        
-        include $classe_dao;
-    }
-
-    //include 'classes/' . $class . '.class.php';*/
-});
+});*/
--- App/index.php
+++ App/index.php
@@ -1,5 +1,6 @@
 <?php
 
 //conequitando com o autoload
+include 'config.php';
 include 'autoload.php';
 include 'rotas.php';
